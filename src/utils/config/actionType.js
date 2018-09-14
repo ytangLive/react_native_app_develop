609feac9c96628bd4f49dc72c63ddea9faa72392
tangyin <devintang@yntengyun.com>
2018-09-14 18:39:44 +0800

    react native 框架搭建

---
+++ src/utils/config/actionType.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ytang on 2018/9/14.
+ */
+
+ /**
+ * action对象的type属性集合
+ */
+const actionType = {
+    FETCH_LOADING_SUFFIX : '_LOADING',
+    FETCH_SUCCESS_SUFFIX : '_SUCCESS',
+    FETCH_ERROR_SUFFIX : '_ERROR',
+    FETCH_SHOW_HUD: 'FETCH_SHOW_HUD',
+
+    MOVIE_LIST: 'MOVIE_LIST',
+    MOVIE_COMEING_NEW_LIST: 'MOVIE_COMEING_NEW_LIST'
+}
+
+export default actionType
